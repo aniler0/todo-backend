f6182832b8c6445e7f1c838b085e26b4425c4db4
aniler0 <anil.er@hotmail.com>
2021-06-22 11:24:37 +0300

    fixed email check

--- src/validation.ts
+++ src/validation.ts
@@ -3,14 +3,14 @@ import Joi from "@hapi/joi";
 const registerValidation = (data: any) => {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
-    email: Joi.string().min(10).required(),
+    email: Joi.string().min(10).required().email(),
     password: Joi.string().min(6).required(),
   });
   return schema.validate(data);
 };
 const loginValidation = (data: any) => {
   const schema = Joi.object({
-    email: Joi.string().min(10).required(),
+    email: Joi.string().min(10).required().email(),
     password: Joi.string().min(6).required(),
   });
   return schema.validate(data);
